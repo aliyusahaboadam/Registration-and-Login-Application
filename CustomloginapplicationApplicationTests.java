896f5e33843b431b6a1690ab392acfbe41b77863
aliyusahaboadam <139462078+aliyusahaboadam@users.noreply.github.com>
2023-07-13 12:58:37 +0100

    Add files via upload

---
+++ CustomloginapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package customloginapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomloginapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
